020a336ba03f5d60e2ee216052a2d3267faa012b
pychangeric <pychangeric@gmail.com>
2023-05-29 14:58:40 +0300

    done

--- lib/student.rb
+++ lib/student.rb
@@ -2,6 +2,7 @@ class Student
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
+
   
   attr_accessor :name, :grade
   
@@ -50,3 +51,6 @@ class Student
   end
 
 end
+
+
+
